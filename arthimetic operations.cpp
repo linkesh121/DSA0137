40e0f782ec0228df1cab636326f47959085bfe36
linkesh121 <112850644+linkesh121@users.noreply.github.com>
2022-09-05 14:08:03 +0530

    Add files via upload

---
+++ arthimetic operations.cpp
@@ -0,0 +1,20 @@
+// online c++ compiler to run c++ program online
+#include <iostream>
+
+int main(){
+	int a,b,c,d,e,f,g;
+	a=9;
+	b=3;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	std:: cout <<"arthi operations add=" <<c<<" \n";
+    std:: cout <<"arthi operations sub=" <<d<<" \n";
+    std:: cout <<"arthi operations multiple=" <<e<<" \n";
+	std:: cout <<"arthi operations division=" <<f<<" \n";
+	std:: cout <<"arthi operations modules=" <<g<<" \n";
+	return 0;
+}
+	
